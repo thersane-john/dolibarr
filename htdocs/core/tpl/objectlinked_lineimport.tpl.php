521915ea2b9ea1c1f2a44f7e5d4d5886e956991e
John BOTELLA <68917336+thersane-john@users.noreply.github.com>
2025-12-18 13:28:10 +0100

    Fix import devis to devis

--- htdocs/core/tpl/objectlinked_lineimport.tpl.php
+++ htdocs/core/tpl/objectlinked_lineimport.tpl.php
@@ -31,8 +31,8 @@ if ($object->element == 'propal') {
 <!-- START TEMPLATE IMPORT OBJECT LINKED LINES -->
 <script>
 
-$(document).ready(function(){
-	$('.objectlinked_importbtn').click(function (e) {
+$(function() {
+	$(document).on('click', '.objectlinked_importbtn', function(e) {
 
 		e.preventDefault();
 		var page = $(this).attr("href");
@@ -75,11 +75,12 @@ $(document).ready(function(){
 				title: "<?php echo $langs->transnoentities('LinesToImport'); ?>",
 				buttons: {
 						"<?php echo $langs->trans('Import'); ?>": function() {
-							  $( this ).dialog( "close" );
-							  $("#" + formId).append('<input type="hidden" name="action" value="import_lines_from_object" />');
-							  $("#" + formId).append('<input type="hidden" name="fromelement" value="' + fromelement + '" />');
-							  $("#" + formId).append('<input type="hidden" name="fromelementid" value="' + fromelementid + '" />');
-							  $("#" + formId).submit();
+							$( this ).dialog( "close" );
+							$("#" + formId).append('<input type="hidden" name="action" value="import_lines_from_object" />');
+							$("#" + formId).append('<input type="hidden" name="fromelement" value="' + fromelement + '" />');
+							$("#" + formId).append('<input type="hidden" name="token" value="<?php print dol_escape_htmltag(newToken()); ?>" />');
+							$("#" + formId).append('<input type="hidden" name="fromelementid" value="' + fromelementid + '" />');
+							$("#" + formId).trigger('submit');
 						},
 						"<?php echo $langs->trans("Cancel"); ?>": function() {
 						  $( this ).dialog( "close" );
